1dd7f593a04e3c72692a71efb2841755697b07b5
Pere <pere.solaclaver@gmail.com>
2020-03-24 14:59:07 +0000

    MUL operation

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -83,6 +83,8 @@ class CPU:
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         # elif op == "SUB": etc
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -111,6 +113,7 @@ class CPU:
         HLT = 0b00000001
         PRN = 0b01000111
         LDI = 0b10000010
+        MUL = 0b10100010
 
         running = True
 
@@ -131,6 +134,10 @@ class CPU:
                 value = self.reg[operand_a]
                 print(value)
                 inc_size = 2
+            # MUL
+            elif IR == MUL:
+                self.alu("MUL", operand_a, operand_b)
+                inc_size = 3
             # HLT
             elif IR == HLT:
                 print("Operations halted.")
